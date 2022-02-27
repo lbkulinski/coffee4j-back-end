6c448d129667bccb2ab36fad5209f0aa1df6dd0b
Logan Kulinski <lbkulinski@users.noreply.github.com>
2022-02-26 23:23:56 -0600

    Added schema creation support

--- src/main/java/com/coffee4j/SchemaController.java
+++ src/main/java/com/coffee4j/SchemaController.java
@@ -7,12 +7,10 @@ import org.bson.conversions.Bson;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/schema")
@@ -22,6 +20,75 @@ public final class SchemaController {
 
     @Value("${spring.data.mongodb.database}")
     private String databaseName;
+
+    @PostMapping("create")
+    public ResponseEntity<Document> create(@RequestBody Map<String, Object> parameters) {
+        System.out.println(parameters);
+
+        String userIdKey = "userId";
+
+        if (!parameters.containsKey(userIdKey)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } //end if
+
+        Object userIdObject = parameters.get(userIdKey);
+
+        if (!(userIdObject instanceof Number number)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } //end if
+
+        long userId = number.longValue();
+
+        String defaultKey = "default";
+
+        if (!parameters.containsKey(defaultKey)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } //end if
+
+        Object defaultFlagObject = parameters.get(defaultKey);
+
+        if (!(defaultFlagObject instanceof Boolean defaultFlag)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } //end if
+
+        String fieldsKey = "fields";
+
+        if (!parameters.containsKey(fieldsKey)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } //end if
+
+        Object fieldsObject = parameters.get(fieldsKey);
+
+        if (!(fieldsObject instanceof Map fields)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } //end if
+
+        MongoClient client = MongoClients.create(this.uri);
+
+        MongoDatabase database = client.getDatabase(this.databaseName);
+
+        String collectionName = "schemas";
+
+        MongoCollection<Document> collection = database.getCollection(collectionName);
+
+        Document schemaDocument = new Document();
+
+        schemaDocument.put(userIdKey, userId);
+
+        schemaDocument.put(defaultKey, defaultFlag);
+
+        schemaDocument.put(fieldsKey, fields);
+
+        collection.insertOne(schemaDocument);
+
+        String successKey = "success";
+
+        boolean successValue = true;
+
+        Document responseDocument = new Document(successKey, successValue);
+
+        return new ResponseEntity<>(responseDocument, HttpStatus.OK);
+    } //create
 
     @GetMapping("read")
     public ResponseEntity<List<Document>> read(@RequestParam long userId) {
