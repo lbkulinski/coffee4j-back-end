6cbbcbad6ae04844ef25e19d2a23b2d927d274fb
Logan Kulinski <lbkulinski@users.noreply.github.com>
2022-03-11 16:56:56 -0600

    Utilities updates

--- src/main/java/com/coffee4j/Utilities.java
+++ src/main/java/com/coffee4j/Utilities.java
@@ -15,11 +15,26 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 
+/**
+ * A set of utility methods used by the Coffee4j application.
+ *
+ * @author Logan Kulinski, lbkulinski@icloud.com
+ * @version March 11, 2022
+ */
 public final class Utilities {
+    /**
+     * The {@link Logger} of the {@link Utilities} class.
+     */
     private static final Logger LOGGER;
 
+    /**
+     * The database URI of the {@link Utilities} class.
+     */
     private static final String URI;
 
+    /**
+     * The database name of the {@link Utilities} class.
+     */
     public static final String DATABASE_NAME;
 
     static {
@@ -58,10 +73,28 @@ public final class Utilities {
         DATABASE_NAME = databaseName;
     } //static
 
+    /**
+     * Throws an {@link InstantiationException}, as an instance of the {@link Utilities} class cannot be created.
+     *
+     * @throws InstantiationException if this constructor is invoked, as an instance of the {@link Utilities} class
+     * cannot be created
+     */
     private Utilities() throws InstantiationException {
         throw new InstantiationException("an instance of the Utilities class cannot be created");
     } //Utilities
 
+    /**
+     * Returns the parameter from the specified parameters that is associated with the specified key and is of the
+     * specified {@link Class} or {@code null} if one does not exist.
+     *
+     * @param parameters the parameters to be used in the operation
+     * @param key the key to be used in the operation
+     * @param clazz the {@link Class} to be used in the operation
+     * @param <T> the type to be used in the operation
+     * @return the parameter from the specified parameters that is associated with the specified key and is of the
+     * specified {@link Class} or {@code null} if one does not exist
+     * @throws NullPointerException if the specified {@link Map} of parameters, key, or {@link Class} is {@code null}
+     */
     public static <T> T getParameter(Map<String, Object> parameters, String key, Class<T> clazz) {
         Objects.requireNonNull(parameters, "the specified Map of parameters is null");
 
@@ -84,6 +117,13 @@ public final class Utilities {
         return clazz.cast(object);
     } //getValue
 
+    /**
+     * Returns the MongoDB collection with the specified collection name.
+     *
+     * @param collectionName the collection name to be used in the operation
+     * @return the MongoDB collection with the specified collection name
+     * @throws NullPointerException if the specified collection name is {@code null}
+     */
     public static MongoCollection<Document> getCollection(String collectionName) {
         Objects.requireNonNull(collectionName, "the specified collection name is null");
 
