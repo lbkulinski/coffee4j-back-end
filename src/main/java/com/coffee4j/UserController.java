c6f65abe74d98c8388c1e9809e9453fd87c6250c
Logan Kulinski <lbkulinski@users.noreply.github.com>
2022-03-11 14:23:17 -0600

    UserController updates

--- src/main/java/com/coffee4j/UserController.java
+++ src/main/java/com/coffee4j/UserController.java
@@ -1,10 +1,17 @@
 package com.coffee4j;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -19,7 +26,12 @@ public final class UserController {
         String firstName = Utilities.getParameter(parameters, firstNameKey, String.class);
 
         if (firstName == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            Map<String, Object> errorMap = Map.of(
+                "success", false,
+                "message", "A first name is required"
+            );
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
         } //end if
 
         firstName = firstName.strip();
@@ -33,7 +45,12 @@ public final class UserController {
         String lastName = Utilities.getParameter(parameters, lastNameKey, String.class);
 
         if (lastName == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            Map<String, Object> errorMap = Map.of(
+                "success", false,
+                "message", "A last name is required"
+            );
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
         } //end if
 
         lastName = lastName.strip();
@@ -45,7 +62,12 @@ public final class UserController {
         String email = Utilities.getParameter(parameters, emailKey, String.class);
 
         if (email == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            Map<String, Object> errorMap = Map.of(
+                "success", false,
+                "message", "An email is required"
+            );
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
         } //end if
 
         email = email.strip();
@@ -62,4 +84,61 @@ public final class UserController {
 
         return new ResponseEntity<>(successMap, HttpStatus.OK);
     } //create
+
+    @GetMapping("read")
+    public ResponseEntity<?> read(@RequestParam Map<String, Object> parameters) {
+        String idKey = "id";
+
+        String id = Utilities.getParameter(parameters, idKey, String.class);
+
+        if (id == null) {
+            Map<String, Object> errorMap = Map.of(
+                "success", false,
+                "message", "An ID is required"
+            );
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        } //end if
+
+        id = id.strip();
+
+        ObjectId objectId;
+
+        try {
+            objectId = new ObjectId(id);
+        } catch (IllegalArgumentException e) {
+            Map<String, Object> errorMap = Map.of(
+                "success", false,
+                "message", "The given ID has an invalid hexadecimal representation"
+            );
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        } //end try catch
+
+        String fieldName = "_id";
+
+        Bson filter = Filters.eq(fieldName, objectId);
+
+        MongoCollection<Document> collection = Utilities.getCollection(UserController.COLLECTION_NAME);
+
+        FindIterable<Document> iterable = collection.find(filter);
+
+        Document userDocument = iterable.first();
+
+        if (userDocument == null) {
+            Map<String, Object> errorMap = Map.of(
+                "success", false,
+                "message", "A user with the given ID could not be found"
+            );
+
+            return new ResponseEntity<>(errorMap, HttpStatus.OK);
+        } //end if
+
+        Map<String, Object> successMap = Map.of(
+            "success", true,
+            "user", userDocument
+        );
+
+        return new ResponseEntity<>(successMap, HttpStatus.OK);
+    } //read
 }
