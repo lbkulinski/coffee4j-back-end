b1cf4b0e024ed35071fb9f0a4be8795a5f8bc45e
Logan Kulinski <lbkulinski@users.noreply.github.com>
2022-03-16 21:23:47 -0500

    Revert "Controller updates"
    This reverts commit e413784df48d06ea24fbc661998c3031478d6206.

--- src/main/java/com/coffee4j/Utilities.java
+++ src/main/java/com/coffee4j/Utilities.java
@@ -14,7 +14,6 @@ import java.nio.file.Path;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
-import java.util.Set;
 
 /**
  * A set of utility methods used by the Coffee4j application.
@@ -37,11 +36,6 @@ public final class Utilities {
      * The database name of the {@link Utilities} class.
      */
     public static final String DATABASE_NAME;
-
-    /**
-     * The valid field types of the {@link Utilities} class.
-     */
-    public static final Set<String> VALID_FIELD_TYPES;
 
     static {
         LOGGER = LogManager.getLogger();
@@ -77,14 +71,6 @@ public final class Utilities {
         URI = uri;
 
         DATABASE_NAME = databaseName;
-
-        VALID_FIELD_TYPES = Set.of(
-            "int",
-            "double",
-            "string",
-            "bool",
-            "date"
-        );
     } //static
 
     /**
