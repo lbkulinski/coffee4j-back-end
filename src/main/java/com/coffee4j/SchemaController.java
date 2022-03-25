e29ecdaa1f314c10e276688c668b8c8696cf23a8
Logan Kulinski <lbkulinski@users.noreply.github.com>
2022-03-25 14:24:02 -0500

    Controller updates

--- src/main/java/com/coffee4j/SchemaController.java
+++ src/main/java/com/coffee4j/SchemaController.java
@@ -165,4 +165,90 @@ public final class SchemaController {
 
         return new ResponseEntity<>(responseMap, HttpStatus.OK);
     } //create
+
+    /**
+     * Attempts to read existing schema data using the specified parameters. Valid filters include a schema's id,
+     * creator ID, default flag, and shared flag. A creator ID must be specified in order to use the default flag
+     * filter. At least one filter is required. Assuming fields with the specified filters exist, a schema's ...
+     *
+     * @param parameters the parameters to be used in the operation
+     * @return a {@link ResponseEntity} containing the outcome of the read operation
+     */
+    @GetMapping("read")
+    public ResponseEntity<Map<String, ?>> read(@RequestParam Map<String, Object> parameters) {
+        String idKey = "id";
+
+        String id = Utilities.getParameter(parameters, idKey, String.class);
+
+        Set<String> whereSubclauses = new HashSet<>();
+
+        List<String> whereArguments = new ArrayList<>();
+
+        if (id != null) {
+            String whereSubclause = "(`id` = ?)";
+
+            whereSubclauses.add(whereSubclause);
+
+            whereArguments.add(id);
+        } //end if
+
+        String creatorIdKey = "creator_id";
+
+        String creatorId = Utilities.getParameter(parameters, creatorIdKey, String.class);
+
+        if (creatorId != null) {
+            String whereSubclause = "(`creator_id` = ?)";
+
+            whereSubclauses.add(whereSubclause);
+
+            whereArguments.add(creatorId);
+        } //end if
+
+        String defaultFlagKey = "default";
+
+        Boolean defaultFlag = Utilities.getParameter(parameters, defaultFlagKey, Boolean.class);
+
+        if ((creatorId == null) && (defaultFlag != null)) {
+            Map<String, ?> errorMap = Map.of(
+                "success", false,
+                "message", "A creator ID is required to use the default flag filter"
+            );
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        } else if (defaultFlag != null) {
+            String whereSubclause = "(`default` = ?)";
+
+            whereSubclauses.add(whereSubclause);
+
+            String defaultFlagString = defaultFlag ? "1" : "0";
+
+            whereArguments.add(defaultFlagString);
+        } //end if
+
+        String sharedFlagKey = "shared";
+
+        if (parameters.containsKey(sharedFlagKey)) {
+            String whereSubclause = "(`shared` = '1')";
+
+            whereSubclauses.add(whereSubclause);
+        } //end if
+
+        if (whereSubclauses.isEmpty()) {
+            Map<String, ?> errorMap = Map.of(
+                "success", false,
+                "message", "At least one filter is required"
+            );
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        } //end if
+
+        String whereSubclausesString = whereSubclauses.stream()
+                                                      .reduce("%s\nAND %s"::formatted)
+                                                      .get();
+
+        String schemaQuery = """
+            """;
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    } //read
 }
