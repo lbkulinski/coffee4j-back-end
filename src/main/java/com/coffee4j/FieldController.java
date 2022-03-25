35d47295cba0c3133f79d83e89b8c4ae0ae3cea9
Logan Kulinski <lbkulinski@users.noreply.github.com>
2022-03-25 10:15:40 -0500

    Controller updates

--- src/main/java/com/coffee4j/FieldController.java
+++ src/main/java/com/coffee4j/FieldController.java
@@ -4,10 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.*;
 import java.util.*;
@@ -16,7 +13,7 @@ import java.util.*;
  * The REST controller used to interact with the Coffee4j field data.
  *
  * @author Logan Kulinski, lbkulinski@icloud.com
- * @version March 24, 2022
+ * @version March 25, 2022
  */
 @RestController
 @RequestMapping("api/fields")
@@ -251,4 +248,379 @@ public class FieldController {
 
         return new ResponseEntity<>(responseMap, HttpStatus.OK);
     } //create
+
+    /**
+     * Attempts to read existing field data using the specified parameters. Valid filters include a field's id, name,
+     * type ID, and display name. Assuming fields with the specified filters exist, a field's name, display name, type
+     * ID and type name are returned.
+     *
+     * @param parameters the parameters to be used in the operation
+     * @return a {@link ResponseEntity} containing the outcome of the read operation
+     */
+    @GetMapping("read")
+    public ResponseEntity<Map<String, ?>> read(@RequestParam Map<String, Object> parameters) {
+        String idKey = "id";
+
+        String id = Utilities.getParameter(parameters, idKey, String.class);
+
+        Set<String> whereSubclauses = new HashSet<>();
+
+        List<String> whereArguments = new ArrayList<>();
+
+        if (id != null) {
+            String whereSubclause = "(`f`.`id` = ?)";
+
+            whereSubclauses.add(whereSubclause);
+
+            whereArguments.add(id);
+        } //end if
+
+        String nameKey = "name";
+
+        String name = Utilities.getParameter(parameters, nameKey, String.class);
+
+        if (name != null) {
+            String whereSubclause = "(`f`.`name` = ?)";
+
+            whereSubclauses.add(whereSubclause);
+
+            whereArguments.add(name);
+        } //end if
+
+        String typeIdKey = "type_id";
+
+        String typeId = Utilities.getParameter(parameters, typeIdKey, String.class);
+
+        if (typeId != null) {
+            String whereSubclause = "(`f`.`type_id` = ?)";
+
+            whereSubclauses.add(whereSubclause);
+
+            whereArguments.add(typeId);
+        } //end if
+
+        String displayNameKey = "display_name";
+
+        String displayName = Utilities.getParameter(parameters, displayNameKey, String.class);
+
+        if (displayName != null) {
+            String whereSubclause = "(`f`.`display_name` = ?)";
+
+            whereSubclauses.add(whereSubclause);
+
+            whereArguments.add(displayName);
+        } //end if
+
+        Connection connection = Utilities.getConnection();
+
+        if (connection == null) {
+            Map<String, ?> errorMap = Map.of(
+                    "success", false,
+                    "message", "The field's data could not be updated"
+            );
+
+            return new ResponseEntity<>(errorMap, HttpStatus.INTERNAL_SERVER_ERROR);
+        } //end if
+
+        String fieldQuery;
+
+        if (whereSubclauses.isEmpty()) {
+            fieldQuery = """
+                SELECT
+                    `f`.`name` AS `field_name`,
+                    `f`.`display_name` AS `field_display_name`,
+                    `ft`.`id` AS `type_id`,
+                    `ft`.`name` AS `type_name`
+                FROM
+                    `fields` `f`
+                        INNER JOIN
+                    `field_types` `ft` ON `ft`.`id` = `f`.`type_id`""";
+        } else {
+            String whereSubclausesString = whereSubclauses.stream()
+                                                          .reduce("%s\nAND%s"::formatted)
+                                                          .get();
+
+            fieldQuery = """
+                SELECT
+                    `f`.`name` AS `field_name`,
+                    `f`.`display_name` AS `field_display_name`,
+                    `ft`.`id` AS `type_id`,
+                    `ft`.`name` AS `type_name`
+                FROM
+                    `fields` `f`
+                        INNER JOIN
+                    `field_types` `ft` ON `ft`.`id` = `f`.`type_id`
+                WHERE
+                    %s""".formatted(whereSubclausesString);
+        } //end if
+
+        PreparedStatement preparedStatement = null;
+
+        ResultSet resultSet = null;
+
+        List<Map<String, String>> fields = new ArrayList<>();
+
+        try {
+            preparedStatement = connection.prepareStatement(fieldQuery);
+
+            for (int i = 0; i < whereArguments.size(); i++) {
+                int parameterIndex = i + 1;
+
+                String argument = whereArguments.get(i);
+
+                preparedStatement.setString(parameterIndex, argument);
+            } //end for
+
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String rowFieldName = resultSet.getString("field_name");
+
+                if (rowFieldName == null) {
+                    continue;
+                } //end if
+
+                String rowFieldDisplayName = resultSet.getString("field_display_name");
+
+                if (rowFieldDisplayName == null) {
+                    continue;
+                } //end if
+
+                String rowTypeId = resultSet.getString("type_id");
+
+                if (rowTypeId == null) {
+                    continue;
+                } //end if
+
+                String rowTypeName = resultSet.getString("type_name");
+
+                if (rowTypeName == null) {
+                    continue;
+                } //end if
+
+                Map<String, String> field = Map.of(
+                    "field_name", rowFieldName,
+                    "field_display_name", rowFieldDisplayName,
+                    "type_id", rowTypeId,
+                    "type_name", rowTypeName
+                );
+
+                fields.add(field);
+            } //end while
+        } catch (SQLException e) {
+            FieldController.LOGGER.atError()
+                                  .withThrowable(e)
+                                  .log();
+
+            Map<String, ?> errorMap = Map.of(
+                "success", false,
+                "message", "The field's data could not be retrieved"
+            );
+
+            return new ResponseEntity<>(errorMap, HttpStatus.INTERNAL_SERVER_ERROR);
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                FieldController.LOGGER.atError()
+                                      .withThrowable(e)
+                                      .log();
+            } //end try catch
+
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    FieldController.LOGGER.atError()
+                                          .withThrowable(e)
+                                          .log();
+                } //end try catch
+            } //end if
+
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    FieldController.LOGGER.atError()
+                                          .withThrowable(e)
+                                          .log();
+                } //end try catch
+            } //end if
+        } //end try catch finally
+
+        Map<String, ?> responseMap;
+
+        if (fields.isEmpty()) {
+            responseMap = Map.of(
+                "success", false,
+                "message", "A field with the specified filters could not be found"
+            );
+        } else {
+            responseMap = Map.of(
+                "success", true,
+                "fields", fields
+            );
+        } //end if
+
+        return new ResponseEntity<>(responseMap, HttpStatus.OK);
+    } //read
+
+    /**
+     * Attempts to update existing field data using the specified parameters. An ID is required for updating. A field's
+     * name, type ID, and display name can be updated. At least one is required.
+     *
+     * @param parameters the parameters to be used in the operation
+     * @return a {@link ResponseEntity} containing the outcome of the update operation
+     */
+    @PostMapping("update")
+    public ResponseEntity<Map<String, ?>> update(@RequestBody Map<String, Object> parameters) {
+        String idKey = "id";
+
+        String id = Utilities.getParameter(parameters, idKey, String.class);
+
+        if (id == null) {
+            Map<String, ?> errorMap = Map.of(
+                "success", false,
+                "message", "An ID is required"
+            );
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        } //end if
+
+        String nameKey = "name";
+
+        String name = Utilities.getParameter(parameters, nameKey, String.class);
+
+        Set<String> setStatements = new HashSet<>();
+
+        List<String> arguments = new ArrayList<>();
+
+        if (name != null) {
+            String setStatement = "    `name` = ?";
+
+            setStatements.add(setStatement);
+
+            arguments.add(name);
+        } //end if
+
+        String typeIdKey = "type_id";
+
+        String typeId = Utilities.getParameter(parameters, typeIdKey, String.class);
+
+        if (typeId != null) {
+            String setStatement = "    `type_id` = ?";
+
+            setStatements.add(setStatement);
+
+            arguments.add(typeId);
+        } //end if
+
+        String displayNameKey = "display_name";
+
+        String displayName = Utilities.getParameter(parameters, displayNameKey, String.class);
+
+        if (displayName != null) {
+            String setStatement = "    `display_name` = ?";
+
+            setStatements.add(setStatement);
+
+            arguments.add(displayName);
+        } //end if
+
+        if (setStatements.isEmpty()) {
+            Map<String, ?> errorMap = Map.of(
+                "success", false,
+                "message", "At lease one update is required"
+            );
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        } //end if
+
+        Connection connection = Utilities.getConnection();
+
+        if (connection == null) {
+            Map<String, ?> errorMap = Map.of(
+                "success", false,
+                "message", "The field's data could not be updated"
+            );
+
+            return new ResponseEntity<>(errorMap, HttpStatus.INTERNAL_SERVER_ERROR);
+        } //end if
+
+        arguments.add(id);
+
+        String updateFieldStatementTemplate = """
+            UPDATE `fields`
+            SET
+            %s
+            WHERE
+                `id` = ?""";
+
+        String setStatementsString = setStatements.stream().reduce("%s,\n%s"::formatted).get();
+
+        String updateFieldStatement = updateFieldStatementTemplate.formatted(setStatementsString);
+
+        PreparedStatement preparedStatement = null;
+
+        int rowsChanged;
+
+        try {
+            preparedStatement = connection.prepareStatement(updateFieldStatement);
+
+            for (int i = 0; i < arguments.size(); i++) {
+                int parameterIndex = i + 1;
+
+                String argument = arguments.get(i);
+
+                preparedStatement.setString(parameterIndex, argument);
+            } //end for
+
+            rowsChanged = preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            FieldController.LOGGER.atError()
+                                  .withThrowable(e)
+                                  .log();
+
+            Map<String, ?> errorMap = Map.of(
+                "success", false,
+                "message", "The field's data could not be updated"
+            );
+
+            return new ResponseEntity<>(errorMap, HttpStatus.INTERNAL_SERVER_ERROR);
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                FieldController.LOGGER.atError()
+                                      .withThrowable(e)
+                                      .log();
+            } //end try catch
+
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    FieldController.LOGGER.atError()
+                                          .withThrowable(e)
+                                          .log();
+                } //end try catch
+            } //end if
+        } //end try catch finally
+
+        Map<String, ?> responseMap;
+
+        if (rowsChanged == 0) {
+            responseMap = Map.of(
+                "success", false,
+                "message", "The field's data could not be updated"
+            );
+        } else {
+            responseMap = Map.of(
+                "success", true,
+                "message", "The field's data was successfully updated"
+            );
+        } //end if
+
+        return new ResponseEntity<>(responseMap, HttpStatus.OK);
+    } //update
 }
