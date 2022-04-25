455aaba32fe9cf105a6877a81edac4de765286ac
Logan Kulinski <lbkulinski@users.noreply.github.com>
2022-04-24 20:58:36 -0700

    Controller updates

--- src/main/java/com/coffee4j/controller/FieldController.java
+++ src/main/java/com/coffee4j/controller/FieldController.java
@@ -1,30 +1,46 @@
 package com.coffee4j.controller;
 
-import com.coffee4j.Body;
-import com.coffee4j.Utilities;
-import com.coffee4j.security.User;
-import org.apache.logging.log4j.LogManager;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.apache.logging.log4j.Logger;
-import org.jooq.*;
+import org.apache.logging.log4j.LogManager;
+import org.jooq.Table;
 import org.jooq.Record;
-import org.jooq.exception.DataAccessException;
 import org.jooq.impl.DSL;
-import org.jooq.impl.SQLDataType;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
+import org.jooq.Field;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import com.coffee4j.Utilities;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
 import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Objects;
+import org.jooq.exception.DataAccessException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.ResponseEntity;
+import com.coffee4j.Body;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.coffee4j.security.User;
+import org.springframework.http.HttpStatus;
+import org.jooq.DataType;
+import org.jooq.impl.SQLDataType;
+import java.net.URI;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.jooq.Condition;
+import org.jooq.Result;
 import java.util.Set;
+import java.util.Map;
 import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.PutMapping;
+import java.util.HashMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
+/**
+ * The REST controller used to interact with the Coffee4j field data.
+ *
+ * @author Logan Kulinski, lbkulinski@gmail.com
+ * @version April 24, 2022
+ */
 @RestController
 @RequestMapping("api/fields")
 public final class FieldController {
@@ -44,7 +60,13 @@ public final class FieldController {
         LOGGER = LogManager.getLogger();
     } //static
 
-    private boolean checkTypeId(int typeId) {
+    /**
+     * Returns whether the specified type ID is invalid.
+     *
+     * @param typeId the type ID to be used in the operation
+     * @return {@code true}, if the specified type ID is invalid and {@code false} otherwise
+     */
+    private boolean typeIdInvalid(int typeId) {
         Table<Record> fieldTypesTable = DSL.table("`field_types`");
 
         Field<Integer> idField = DSL.field("`id`", Integer.class);
@@ -61,18 +83,28 @@ public final class FieldController {
         } catch (SQLException | DataAccessException e) {
             LOGGER.atError().withThrowable(e).log();
 
-            return false;
+            return true;
         } //end try catch
 
         if (record == null) {
-            return false;
+            return true;
         } //end if
 
         int count = record.get(0, Integer.class);
 
-        return count == 1;
-    } //checkTypeId
+        return count == 0;
+    } //typeIdInvalid
 
+    /**
+     * Attempts to create a new field for the current logged-in user. A name, type ID, display name, and shared flag
+     * required for creation.
+     *
+     * @param name the name to be used in the operation
+     * @param typeId the type ID to be used in the operation
+     * @param displayName the display name to be used in the operation
+     * @param sharedFlag the shared flag to be used in the operation
+     * @return a {@link ResponseEntity} containing the outcome of the create operation
+     */
     @PostMapping
     public ResponseEntity<Body<?>> create(@RequestParam String name, @RequestParam("type_id") int typeId,
                                           @RequestParam("display_name") String displayName,
@@ -95,11 +127,7 @@ public final class FieldController {
             Body<String> body = Body.error(content);
 
             return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
-        } //end if
-
-        boolean valid = this.checkTypeId(typeId);
-
-        if (!valid) {
+        } else if (this.typeIdInvalid(typeId)) {
             String content = "%d is not a valid type ID".formatted(typeId);
 
             Body<String> body = Body.error(content);
@@ -172,6 +200,18 @@ public final class FieldController {
         return new ResponseEntity<>(body, httpHeaders, HttpStatus.CREATED);
     } //create
 
+    /**
+     * Attempts to read the field data of the current logged-in user. Assuming data exists, the ID, creator ID, name,
+     * type ID, display name, and shared flag of each field are returned.
+     *
+     * @param id the ID to be used in the operation
+     * @param creatorId the creator ID to be used in the operation
+     * @param name the name to be used in the operation
+     * @param typeId the type ID to be used in the operation
+     * @param displayName the display name to be used in the operation
+     * @param sharedFlag the shared flag to be used in the operation
+     * @return a {@link ResponseEntity} containing the outcome of the read operation
+     */
     @GetMapping
     public ResponseEntity<Body<?>> read(@RequestParam(required = false) Integer id,
                                         @RequestParam(name = "creator_id", required = false) Integer creatorId,
@@ -270,4 +310,183 @@ public final class FieldController {
 
         return new ResponseEntity<>(body, HttpStatus.OK);
     } //read
+
+    /**
+     * Attempts to update the field data of the current logged-in user. A field's name, type ID, display name, and
+     * shared flag can be updated. An ID and at least one update are required.
+     *
+     * @param id the ID to be used in the operation
+     * @param name the name to be used in the operation
+     * @param typeId the type ID to be used in the operation
+     * @param displayName the display name to be used in the operation
+     * @param sharedFlag the shared flag to be used in the operation
+     * @return a {@link ResponseEntity} containing the outcome of the update operation
+     */
+    @PutMapping
+    public ResponseEntity<Body<?>> update(@RequestParam int id, @RequestParam(required = false) String name,
+                                          @RequestParam(name = "type_id", required = false) Integer typeId,
+                                          @RequestParam(name = "display_name", required = false) String displayName,
+                                          @RequestParam(name = "shared", required = false) Boolean sharedFlag) {
+        User user = Utilities.getLoggedInUser();
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        } //end if
+
+        if ((name != null) && (name.length() > MAX_NAME_LENGTH)) {
+            String content = "A name cannot exceed %d characters".formatted(MAX_NAME_LENGTH);
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        } else if ((displayName != null) && (displayName.length() > MAX_NAME_LENGTH)) {
+            String content = "A display name cannot exceed %d characters".formatted(MAX_NAME_LENGTH);
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        } else if ((typeId != null) && this.typeIdInvalid(typeId)) {
+            String content = "%d is not a valid type ID".formatted(typeId);
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        } //end if
+
+        Map<Field<?>, Object> fieldToNewValue = new HashMap<>();
+
+        if (name != null) {
+            Field<String> nameField = DSL.field("`name`", String.class);
+
+            fieldToNewValue.put(nameField, name);
+        } //end if
+
+        if (typeId != null) {
+            Field<Integer> typeIdField = DSL.field("`type_id`", Integer.class);
+
+            fieldToNewValue.put(typeIdField, typeId);
+        } //end if
+
+        if (displayName != null) {
+            Field<String> displayNameField = DSL.field("`display_name`", String.class);
+
+            fieldToNewValue.put(displayNameField, displayName);
+        } //end if
+
+        if (sharedFlag != null) {
+            Field<Boolean> sharedField = DSL.field("`shared`", Boolean.class);
+
+            fieldToNewValue.put(sharedField, sharedFlag);
+        } //end if
+
+        if (fieldToNewValue.isEmpty()) {
+            String content = "At lease one update is required";
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        } //end if
+
+        Table<Record> fieldsTable = DSL.table("`fields`");
+
+        Field<Integer> idField = DSL.field("`id`", Integer.class);
+
+        Field<Integer> creatorIdField = DSL.field("`creator_id`", Integer.class);
+
+        int creatorId = user.id();
+
+        int rowsChanged;
+
+        try (Connection connection = DriverManager.getConnection(Utilities.DATABASE_URL)) {
+            DSLContext context = DSL.using(connection, SQLDialect.MYSQL);
+
+            rowsChanged = context.update(fieldsTable)
+                                 .set(fieldToNewValue)
+                                 .where(idField.eq(id))
+                                 .and(creatorIdField.eq(creatorId))
+                                 .execute();
+        } catch (SQLException | DataAccessException e) {
+            LOGGER.atError()
+                  .withThrowable(e)
+                  .log();
+
+            String content = "The field's data could not be updated";
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+        } //end try catch
+
+        if (rowsChanged == 0) {
+            String content = "The field's data could not be updated";
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        } //end if
+
+        String content = "The field's data was successfully updated";
+
+        Body<String> body = Body.success(content);
+
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    } //update
+
+    /**
+     * Attempts to delete the field data of the current logged-in user. An ID is required for deletion.
+     *
+     * @param id the ID to be used in the operation
+     * @return a {@link ResponseEntity} containing the outcome of the delete operation
+     */
+    @DeleteMapping
+    public ResponseEntity<Body<?>> delete(@RequestParam int id) {
+        User user = Utilities.getLoggedInUser();
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        } //end if
+
+        Table<Record> fieldsTable = DSL.table("`fields`");
+
+        Field<Integer> idField = DSL.field("`id`", Integer.class);
+
+        Field<Integer> creatorIdField = DSL.field("`creator_id`", Integer.class);
+
+        int creatorId = user.id();
+
+        int rowsChanged;
+
+        try (Connection connection = DriverManager.getConnection(Utilities.DATABASE_URL)) {
+            DSLContext context = DSL.using(connection, SQLDialect.MYSQL);
+
+            rowsChanged = context.delete(fieldsTable)
+                                 .where(idField.eq(id))
+                                 .and(creatorIdField.eq(creatorId))
+                                 .execute();
+        } catch (SQLException | DataAccessException e) {
+            LOGGER.atError()
+                  .withThrowable(e)
+                  .log();
+
+            String content = "The field's data could not be deleted";
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+        } //end try catch
+
+        if (rowsChanged == 0) {
+            String content = "The field's data could not be deleted";
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        } //end if
+
+        String content = "The field's data was successfully deleted";
+
+        Body<String> body = Body.success(content);
+
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    } //delete
 }
