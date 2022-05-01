8fd83020d00164560b9f9ca3e07e24324ac1b032
Logan Kulinski <lbkulinski@users.noreply.github.com>
2022-05-01 13:35:11 -0700

    Controller updates

--- src/main/java/com/coffee4j/controller/SchemaFieldsController.java
+++ src/main/java/com/coffee4j/controller/SchemaFieldsController.java
@@ -10,7 +10,6 @@ import org.jooq.impl.DSL;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import schema.generated.tables.FieldTypes;
 import schema.generated.tables.Fields;
 import schema.generated.tables.SchemaFields;
 import org.apache.logging.log4j.Logger;
@@ -22,6 +21,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.*;
 
+/**
+ * The REST controller used to interact with the Coffee4j schema-field association data.
+ *
+ * @author Logan Kulinski, lbkulinski@gmail.com
+ * @version May 1, 2022
+ */
 @RestController
 @RequestMapping("api/schema_fields")
 public final class SchemaFieldsController {
@@ -34,11 +39,6 @@ public final class SchemaFieldsController {
      * The {@code fields} table of the {@link SchemaFieldsController} class.
      */
     private static final Fields FIELDS;
-
-    /**
-     * The {@code field_types} table of the {@link SchemaFieldsController} class.
-     */
-    private static final FieldTypes FIELD_TYPES;
 
     /**
      * The {@code schema_fields} table of the {@link SchemaFieldsController} class.
@@ -54,8 +54,6 @@ public final class SchemaFieldsController {
         SCHEMAS = Schemas.SCHEMAS;
 
         FIELDS = Fields.FIELDS;
-
-        FIELD_TYPES = FieldTypes.FIELD_TYPES;
 
         SCHEMA_FIELDS = SchemaFields.SCHEMA_FIELDS;
 
@@ -93,6 +91,14 @@ public final class SchemaFieldsController {
         return schemaExists && fieldExists;
     } //checkOwner
 
+    /**
+     * Attempts to create a new schema-field association for the current logged-in user. A schema ID and field ID are
+     * required for creation.
+     *
+     * @param schemaId the schema ID to be used in the operation
+     * @param fieldId the field ID to be used in the operation
+     * @return a {@link ResponseEntity} containing the outcome of the create operation
+     */
     @PostMapping
     public ResponseEntity<Body<?>> create(@RequestParam("schema_id") int schemaId,
                                           @RequestParam("field_id") int fieldId) {
@@ -107,7 +113,7 @@ public final class SchemaFieldsController {
         boolean ownerValid = this.checkOwner(schemaId, fieldId, ownerId);
 
         if (!ownerValid) {
-            String content = "An association with the specified parameters could not be created";
+            String content = "A schema-field association with the specified parameters could not be created";
 
             Body<String> body = Body.error(content);
 
@@ -128,7 +134,7 @@ public final class SchemaFieldsController {
                   .withThrowable(e)
                   .log();
 
-            String content = "An association with the specified parameters could not be created";
+            String content = "A schema-field association with the specified parameters could not be created";
 
             Body<String> body = Body.error(content);
 
@@ -136,164 +142,90 @@ public final class SchemaFieldsController {
         } //end try catch
 
         if (rowsChanged == 0) {
-            String content = "An association with the specified parameters could not be created";
+            String content = "A schema-field association with the specified parameters could not be created";
 
             Body<String> body = Body.error(content);
 
             return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
         } //end if
 
-        String content = "An association with the specified parameters was successfully created";
+        String content = "A schema-field association with the specified parameters was successfully created";
 
         Body<String> body = Body.success(content);
 
         return new ResponseEntity<>(body, HttpStatus.OK);
     } //create
 
-    private void processRecord(Record record, Map<Integer, Map<String, Object>> idToSchema,
-                               Map<Integer, Set<Map<String, Object>>> idToFields) {
-        Objects.requireNonNull(record, "the specified record is null");
-
-        Objects.requireNonNull(idToSchema, "the specified map of ID to schema is null");
-
-        Objects.requireNonNull(idToFields, "the specified map of ID to fields is null");
-
-        int schemaId = record.get(SCHEMAS.ID);
-
-        Map<String, Object> schema = idToSchema.computeIfAbsent(schemaId, id -> new HashMap<>());
-
-        schema.putIfAbsent("id", schemaId);
-
-        int schemaOwnerId = record.get(SCHEMAS.OWNER_ID);
-
-        schema.putIfAbsent("owner_id", schemaOwnerId);
-
-        String schemaName = record.get(SCHEMAS.NAME);
-
-        schema.putIfAbsent("name", schemaName);
-
-        boolean schemaDefault = record.get(SCHEMAS.DEFAULT);
-
-        schema.putIfAbsent("default", schemaDefault);
-
-        boolean schemaShared = record.get(SCHEMAS.SHARED);
-
-        schema.putIfAbsent("shared", schemaShared);
-
-        int fieldTypeId = record.get(FIELD_TYPES.ID);
-
-        String fieldTypeName = record.get(FIELD_TYPES.NAME);
-
-        int fieldId = record.get(FIELDS.ID);
-
-        String fieldName = record.get(FIELDS.NAME);
-
-        String fieldDisplayName = record.get(FIELDS.DISPLAY_NAME);
-
-        boolean fieldShared = record.get(FIELDS.SHARED);
-
-        Map<String, Object> fieldType = Map.of(
-            "id", fieldTypeId,
-            "name", fieldTypeName
-        );
-
-        Map<String, Object> field = Map.of(
-            "id", fieldId,
-            "name", fieldName,
-            "display_name", fieldDisplayName,
-            "shared", fieldShared,
-            "type", fieldType
-        );
-
-        Set<Map<String, Object>> fields = idToFields.computeIfAbsent(schemaId, id -> new HashSet<>());
-
-        fields.add(field);
-    } //processRecord
-
+    /**
+     * Attempts to read the schema-field association data of the current logged-in user. Assuming data exists, the
+     * schema ID and field ID of each schema-field association are returned.
+     *
+     * @param schemaId the schema ID to be used in the operation
+     * @param fieldId the field ID to be used in the operation
+     * @return a {@link ResponseEntity} containing the outcome of the read operation
+     */
     @GetMapping
-    public ResponseEntity<Body<?>> read(@RequestParam(name = "schema_id", required = false) Integer schemaId) {
+    public ResponseEntity<Body<?>> read(@RequestParam(name = "schema_id", required = false) Integer schemaId,
+                                        @RequestParam(name = "field_id", required = false) Integer fieldId) {
         User user = Utilities.getLoggedInUser();
 
         if (user == null) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         } //end if
 
-        List<Field<?>> columns = List.of(
-            SCHEMAS.ID,
-            SCHEMAS.OWNER_ID,
-            SCHEMAS.NAME,
-            SCHEMAS.DEFAULT,
-            SCHEMAS.SHARED,
-            FIELDS.ID,
-            FIELDS.NAME,
-            FIELDS.DISPLAY_NAME,
-            FIELD_TYPES.ID,
-            FIELD_TYPES.NAME
-        );
+        Condition condition = DSL.noCondition();
 
-        Condition condition;
+        if (schemaId != null) {
+            condition = condition.and(SCHEMAS.ID.eq(schemaId));
+        } //end if
 
-        if (schemaId == null) {
-            condition = DSL.noCondition();
-        } else {
-            condition = SCHEMAS.ID.eq(schemaId);
+        if (fieldId != null) {
+            condition = condition.and(FIELDS.ID.eq(fieldId));
         } //end if
 
         int ownerId = user.id();
 
-        Result<Record> result;
+        Result<? extends Record> result;
 
         try (Connection connection = DriverManager.getConnection(Utilities.DATABASE_URL)) {
             DSLContext context = DSL.using(connection, SQLDialect.MYSQL);
 
-            result = context.select(columns)
+            result = context.select(SCHEMA_FIELDS.SCHEMA_ID, SCHEMA_FIELDS.FIELD_ID)
                             .from(SCHEMAS)
                             .join(SCHEMA_FIELDS).on(SCHEMA_FIELDS.SCHEMA_ID.eq(SCHEMAS.ID))
                             .join(FIELDS).on(FIELDS.ID.eq(SCHEMA_FIELDS.FIELD_ID))
-                            .join(FIELD_TYPES).on(FIELD_TYPES.ID.eq(FIELDS.TYPE_ID))
                             .where(condition)
                             .and(SCHEMAS.OWNER_ID.eq(ownerId)
                                                  .or(SCHEMAS.SHARED.isTrue()))
                             .and(FIELDS.OWNER_ID.eq(ownerId)
                                                 .or(FIELDS.SHARED.isTrue()))
                             .fetch();
+
+            System.out.println(context.select(SCHEMA_FIELDS.SCHEMA_ID, SCHEMA_FIELDS.FIELD_ID)
+                                      .from(SCHEMAS)
+                                      .join(SCHEMA_FIELDS).on(SCHEMA_FIELDS.SCHEMA_ID.eq(SCHEMAS.ID))
+                                      .join(FIELDS).on(FIELDS.ID.eq(SCHEMA_FIELDS.FIELD_ID))
+                                      .where(condition)
+                                      .and(SCHEMAS.OWNER_ID.eq(ownerId)
+                                                           .or(SCHEMAS.SHARED.isTrue()))
+                                      .and(FIELDS.OWNER_ID.eq(ownerId)
+                                                          .or(FIELDS.SHARED.isTrue()))
+                                      .getSQL());
         } catch (SQLException | DataAccessException e) {
             LOGGER.atError()
                   .withThrowable(e)
                   .log();
 
-            String content = "The association's data could not be retrieved";
+            String content = "The schema-field association's data could not be retrieved";
 
             Body<String> body = Body.error(content);
 
             return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
         } //end try catch
 
-        Map<Integer, Map<String, Object>> idToSchema = new HashMap<>();
+        List<Map<String, Object>> content = result.intoMaps();
 
-        Map<Integer, Set<Map<String, Object>>> idToFields = new HashMap<>();
-
-        for (Record record : result) {
-            this.processRecord(record, idToSchema, idToFields);
-        } //end for
-
-        for (Map.Entry<Integer, Map<String, Object>> entry : idToSchema.entrySet()) {
-            int id = entry.getKey();
-
-            if (!idToFields.containsKey(id)) {
-                continue;
-            } //end if
-
-            Set<Map<String, Object>> fields = idToFields.get(id);
-
-            Map<String, Object> schema = entry.getValue();
-
-            schema.put("fields", fields);
-        } //end for
-
-        Collection<Map<String, Object>> values = idToSchema.values();
-
-        Body<Collection<Map<String, Object>>> body = Body.success(values);
+        Body<List<Map<String, Object>>> body = Body.success(content);
 
         return new ResponseEntity<>(body, HttpStatus.OK);
     } //read
