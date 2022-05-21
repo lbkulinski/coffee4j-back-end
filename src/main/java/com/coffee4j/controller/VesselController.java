bc74dae0dc4c1f1574599101368907c639a2baba
Logan Kulinski <lbkulinski@users.noreply.github.com>
2022-05-21 12:50:08 -0500

    Controller updates

--- src/main/java/com/coffee4j/controller/VesselController.java
+++ src/main/java/com/coffee4j/controller/VesselController.java
@@ -1,16 +1,33 @@
 package com.coffee4j.controller;
 
+import com.coffee4j.Body;
+import com.coffee4j.Utilities;
+import com.coffee4j.security.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.jooq.*;
+import org.jooq.Record;
+import org.jooq.exception.DataAccessException;
+import org.jooq.impl.DSL;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import schema.generated.tables.Vessel;
+import schema.generated.tables.records.VesselRecord;
+
+import java.net.URI;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * The REST controller used to interact with the Coffee4j vessel data.
  *
  * @author Logan Kulinski, lbkulinski@gmail.com
- * @version May 20, 2022
+ * @version May 21, 2022
  */
 @RestController
 @RequestMapping("/api/vessel")
@@ -30,4 +47,110 @@ public final class VesselController {
 
         LOGGER = LogManager.getLogger();
     } //static
+
+    @PostMapping
+    public ResponseEntity<Body<?>> create(@RequestParam String name) {
+        User user = Utilities.getLoggedInUser();
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        } //end if
+
+        int userId = user.id();
+
+        VesselRecord record;
+
+        try (Connection connection = DriverManager.getConnection(Utilities.DATABASE_URL)) {
+            DSLContext context = DSL.using(connection, SQLDialect.POSTGRES);
+
+            record = context.insertInto(VESSEL)
+                            .columns(VESSEL.USER_ID, VESSEL.NAME)
+                            .values(userId, name)
+                            .returning(VESSEL.ID)
+                            .fetchOne();
+        } catch (SQLException | DataAccessException e) {
+            LOGGER.atError()
+                  .withThrowable(e)
+                  .log();
+
+            String content = "A vessel with the specified parameters could not be created";
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+        } //end try catch
+
+        if (record == null) {
+            String content = "A vessel with the specified parameters could not be created";
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        } //end if
+
+        String content = "A vessel with the specified parameters was successfully created";
+
+        Body<String> body = Body.success(content);
+
+        int id = record.getId();
+
+        String locationString = "http://localhost:8080/api/vessel?id=%d".formatted(id);
+
+        URI location = URI.create(locationString);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        httpHeaders.setLocation(location);
+
+        return new ResponseEntity<>(body, httpHeaders, HttpStatus.OK);
+    } //create
+
+    @GetMapping
+    public ResponseEntity<Body<?>> read(@RequestParam(required = false) Integer id,
+                                        @RequestParam(required = false) String name) {
+        User user = Utilities.getLoggedInUser();
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        } //end if
+
+        int userId = user.id();
+
+        Condition condition = VESSEL.USER_ID.eq(userId);
+
+        if (id != null) {
+            condition = condition.and(VESSEL.ID.eq(id));
+        } //end if
+
+        if (name != null) {
+            condition = condition.and(VESSEL.NAME.eq(name));
+        } //end if
+
+        Result<? extends Record> result;
+
+        try (Connection connection = DriverManager.getConnection(Utilities.DATABASE_URL)) {
+            DSLContext context = DSL.using(connection, SQLDialect.POSTGRES);
+
+            result = context.select(VESSEL.ID, VESSEL.NAME)
+                            .from(VESSEL)
+                            .where(condition)
+                            .fetch();
+        } catch (SQLException | DataAccessException e) {
+            LOGGER.atError()
+                  .withThrowable(e)
+                  .log();
+
+            String content = "A vessel with the specified parameters could not be read";
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+        } //end try catch
+
+        List<Map<String, Object>> content = result.intoMaps();
+
+        Body<List<Map<String, Object>>> body = Body.success(content);
+
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    } //read
 }
