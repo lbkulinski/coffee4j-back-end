572affa7f8316bb890f08278b0dbed8bc781fe95
Logan Kulinski <lbkulinski@users.noreply.github.com>
2022-05-22 12:27:03 -0500

    Controller updates

--- src/main/java/com/coffee4j/controller/BrewController.java
+++ src/main/java/com/coffee4j/controller/BrewController.java
@@ -5,11 +5,8 @@ import com.coffee4j.Utilities;
 import com.coffee4j.security.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jooq.Condition;
-import org.jooq.DSLContext;
+import org.jooq.*;
 import org.jooq.Record;
-import org.jooq.Result;
-import org.jooq.SQLDialect;
 import org.jooq.exception.DataAccessException;
 import org.jooq.impl.DSL;
 import org.springframework.http.HttpHeaders;
@@ -26,24 +23,52 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The REST controller used to interact with the Coffee4j brew data.
+ *
+ * @author Logan Kulinski, lbkulinski@gmail.com
+ * @version May 22, 2022
+ */
 @RestController
 @RequestMapping("/api/brew")
 public final class BrewController {
+    /**
+     * The {@code brew} table of the {@link BrewController} class.
+     */
     private static final Brew BREW;
 
+    /**
+     * The {@code coffee} table of the {@link BrewController} class.
+     */
     private static final Coffee COFFEE;
 
+    /**
+     * The {@code water} table of the {@link BrewController} class.
+     */
     private static final Water WATER;
 
+    /**
+     * The {@code brewer} table of the {@link BrewController} class.
+     */
     private static final Brewer BREWER;
 
+    /**
+     * The {@code filter} table of the {@link BrewController} class.
+     */
     private static final Filter FILTER;
 
+    /**
+     * The {@code vessel} table of the {@link BrewController} class.
+     */
     private static final Vessel VESSEL;
 
+    /**
+     * The {@link Logger} of the {@link BrewController} class.
+     */
     private static final Logger LOGGER;
 
     static {
@@ -62,6 +87,20 @@ public final class BrewController {
         LOGGER = LogManager.getLogger();
     } //static
 
+    /**
+     * Attempts to create a new brew. A timestamp, coffee ID, water ID, brewer ID, filter ID, vessel ID, coffee mass,
+     * and water mass are required for creation.
+     *
+     * @param timestampString the timestamp {@link String} to be used in the operation
+     * @param coffeeId the coffee ID to be used in the operation
+     * @param waterId the water ID to be used in the operation
+     * @param brewerId the brewer ID to be used in the operation
+     * @param filterId the filter ID to be used in the operation
+     * @param vesselId the vessel ID to be used in the operation
+     * @param coffeeMass the coffee mass to be used in the operation
+     * @param waterMass the water mass to be used in the operation
+     * @return a {@link ResponseEntity} containing the outcome of the create operation
+     */
     @PostMapping
     public ResponseEntity<Body<?>> create(@RequestParam("timestamp") String timestampString,
                                           @RequestParam("coffee_id") int coffeeId,
@@ -144,6 +183,23 @@ public final class BrewController {
         return new ResponseEntity<>(body, httpHeaders, HttpStatus.OK);
     } //create
 
+    /**
+     * Attempts to read the brew data of the current logged-in user. An ID, timestamp, coffee ID, water ID, brewer ID,
+     * filter ID, vessel ID, coffee mass, or water mass can be used to filter the data. Assuming data exists, the ID,
+     * timestamp, coffee ID, coffee name, water ID, water name, brewer ID, brew name, filter ID, filter name, vessel
+     * ID, vessel name, coffee mass, and water mass of each brew are returned.
+     *
+     * @param id the ID to be used in the operation
+     * @param timestampString the timestamp {@link String} to be used in the operation
+     * @param coffeeId the coffee ID to be used in the operation
+     * @param waterId the water ID to be used in the operation
+     * @param brewerId the brewer ID to be used in the operation
+     * @param filterId the filter ID to be used in the operation
+     * @param vesselId the vessel ID to be used in the operation
+     * @param coffeeMass the coffee mass to be used in the operation
+     * @param waterMass the water mass to be used in the operation
+     * @return a {@link ResponseEntity} containing the outcome of the read operation
+     */
     @GetMapping
     public ResponseEntity<Body<?>> read(@RequestParam(required = false) Integer id,
                                         @RequestParam(required = false, name = "timestamp") String timestampString,
@@ -251,4 +307,186 @@ public final class BrewController {
 
         return new ResponseEntity<>(body, HttpStatus.OK);
     } //read
+
+    /**
+     * Attempts to update the brew data of the current logged-in user. A brew's timestamp, coffee ID, water ID, brewer
+     * ID, filter ID, vessel ID, coffee mass, and water mass can be updated. An ID and at least one new value are
+     * required for updating.
+     *
+     * @param id the ID to be used in the operation
+     * @param timestampString the timestamp {@link String} to be used in the operation
+     * @param coffeeId the coffee ID to be used in the operation
+     * @param waterId the water ID to be used in the operation
+     * @param brewerId the brewer ID to be used in the operation
+     * @param filterId the filter ID to be used in the operation
+     * @param vesselId the vessel ID to be used in the operation
+     * @param coffeeMass the coffee mass to be used in the operation
+     * @param waterMass the water mass to be used in the operation
+     * @return a {@link ResponseEntity} containing the outcome of the update operation
+     */
+    @PutMapping
+    public ResponseEntity<Body<?>> update(@RequestParam int id,
+                                          @RequestParam(required = false, name = "timestamp") String timestampString,
+                                          @RequestParam(required = false, name = "coffee_id") Integer coffeeId,
+                                          @RequestParam(required = false, name = "water_id") Integer waterId,
+                                          @RequestParam(required = false, name = "brewer_id") Integer brewerId,
+                                          @RequestParam(required = false, name = "filter_id") Integer filterId,
+                                          @RequestParam(required = false, name = "vessel_id") Integer vesselId,
+                                          @RequestParam(required = false, name = "coffee_mass") BigDecimal coffeeMass,
+                                          @RequestParam(required = false, name = "water_mass") BigDecimal waterMass) {
+        User user = Utilities.getLoggedInUser();
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        } //end if
+
+        Map<Field<?>, Object> fieldToNewValue = new HashMap<>();
+
+        if (timestampString != null) {
+            LocalDateTime timestamp;
+
+            try {
+                timestamp = LocalDateTime.parse(timestampString);
+            } catch (DateTimeParseException e) {
+                LOGGER.atError()
+                      .withThrowable(e)
+                      .log();
+
+                String content = "The specified timestamp is malformed";
+
+                Body<String> body = Body.error(content);
+
+                return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+            } //end try catch
+
+            fieldToNewValue.put(BREW.TIMESTAMP, timestamp);
+        } //end if
+
+        if (coffeeId != null) {
+            fieldToNewValue.put(BREW.COFFEE_ID, coffeeId);
+        } //end if
+
+        if (waterId != null) {
+            fieldToNewValue.put(BREW.WATER_ID, waterId);
+        } //end if
+
+        if (brewerId != null) {
+            fieldToNewValue.put(BREW.BREWER_ID, brewerId);
+        } //end if
+
+        if (filterId != null) {
+            fieldToNewValue.put(BREW.FILTER_ID, filterId);
+        } //end if
+
+        if (vesselId != null) {
+            fieldToNewValue.put(BREW.VESSEL_ID, vesselId);
+        } //end if
+
+        if (coffeeMass != null) {
+            fieldToNewValue.put(BREW.COFFEE_MASS, coffeeMass);
+        } //end if
+
+        if (waterMass != null) {
+            fieldToNewValue.put(BREW.WATER_MASS, waterMass);
+        } //end if
+
+        if (fieldToNewValue.isEmpty()) {
+            String content = "At least one value must be updated";
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        } //end if
+
+        int userId = user.id();
+
+        int rowsChanged;
+
+        try (Connection connection = DriverManager.getConnection(Utilities.DATABASE_URL)) {
+            DSLContext context = DSL.using(connection, SQLDialect.POSTGRES);
+
+            rowsChanged = context.update(BREW)
+                                 .set(fieldToNewValue)
+                                 .where(BREW.ID.eq(id))
+                                 .and(BREW.USER_ID.eq(userId))
+                                 .execute();
+        } catch (SQLException | DataAccessException e) {
+            LOGGER.atError()
+                  .withThrowable(e)
+                  .log();
+
+            String content = "A brew with the specified parameters could not be updated";
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+        } //end try catch
+
+        if (rowsChanged == 0) {
+            String content = "A brew with the specified parameters could not be updated";
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        } //end if
+
+        String content = "A brew with the specified parameters was successfully updated";
+
+        Body<String> body = Body.success(content);
+
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    } //update
+
+    /**
+     * Attempts to delete the brew data of the current logged-in user. A single brew can be deleted. An ID is required
+     * for deletion.
+     *
+     * @param id the ID to be used in the operation
+     * @return a {@link ResponseEntity} containing the outcome of the delete operation
+     */
+    @DeleteMapping
+    public ResponseEntity<Body<?>> delete(@RequestParam int id) {
+        User user = Utilities.getLoggedInUser();
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        } //end if
+
+        int userId = user.id();
+
+        int rowsChanged;
+
+        try (Connection connection = DriverManager.getConnection(Utilities.DATABASE_URL)) {
+            DSLContext context = DSL.using(connection, SQLDialect.POSTGRES);
+
+            rowsChanged = context.deleteFrom(BREW)
+                                 .where(BREW.ID.eq(id))
+                                 .and(BREW.USER_ID.eq(userId))
+                                 .execute();
+        } catch (SQLException | DataAccessException e) {
+            LOGGER.atError()
+                  .withThrowable(e)
+                  .log();
+
+            String content = "A brew with the specified parameters could not be deleted";
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+        } //end try catch
+
+        if (rowsChanged == 0) {
+            String content = "A brew with the specified parameters could not be deleted";
+
+            Body<String> body = Body.error(content);
+
+            return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        } //end if
+
+        String content = "A brew with the specified parameters was successfully deleted";
+
+        Body<String> body = Body.success(content);
+
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    } //delete
 }
